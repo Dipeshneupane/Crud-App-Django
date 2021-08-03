76efbfca9cf9ca3784c2271941fcdb21eeaaea7e
Dipesh Neupane <dipeshneupane2055@gmail.com>
2021-08-03 17:51:22 +0545

    User Authentication Feature Added

--- crudproject/enroll/urls.py
+++ crudproject/enroll/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path("", addShow, name = "addShow"),
+    path("addShow", addShow, name = "addShow"),
     path('delete/<int:id>/', deleteData, name = "deleteData"),
     path("<int:id>", updateData, name = 'updateData'),
+    path('', LoginView.as_view(), name = 'login'),
+    path('register', Register, name = 'register'),
 ]
--- crudproject/enroll/views.py
+++ crudproject/enroll/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 from .forms import StudentRegistration
 from .models import User
 from django.core.paginator import Paginator, EmptyPage
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
+
+
+@login_required
 def addShow(request):
  
     # check if the request is post
@@ -29,9 +34,9 @@ def addShow(request):
         page = p.page(page_num)
     except EmptyPage:
         page = p.page(1)
-    return render(request, 'enroll/addandshow.html', {'form': form, 'stu': page})
+    return render(request, 'addandshow.html', {'form': form, 'stu': page})
 
-
+@login_required
 
 def deleteData(request, id):
     if request.method =='POST':
@@ -49,4 +54,18 @@ def updateData(request, id):
         pi = User.objects.get(pk = id)
         fm = StudentRegistration(instance = pi)
 
-    return render(request, 'enroll/updatestudent.html',{'form':fm})
+    return render(request, 'updatestudent.html',{'form':fm})
+
+
+def Register(request):
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print('form is valid')
+            form.save()
+            return HttpResponseRedirect('login')
+        else:
+            print('form is invalid')
+    else:
+        form = UserCreationForm()
+    return render(request,'register.html', {'form':form})
